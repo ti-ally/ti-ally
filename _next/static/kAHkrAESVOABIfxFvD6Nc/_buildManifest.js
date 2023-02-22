f7968d775ed53a8dfb66e59b6ec14d0e3f75335e
peterhs.kang <peterhs.kang@gmail.com>
2023-02-23 05:04:29 +0900

    Updates

---
+++ _next/static/kAHkrAESVOABIfxFvD6Nc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/211-aae4de57fd10f851.js","static/chunks/pages/index-78b0980e9f1ca577.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
